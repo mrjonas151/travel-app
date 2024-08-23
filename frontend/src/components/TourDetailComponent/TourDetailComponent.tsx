189fa64643126a2decdbdee87eb614b5fa16de72
Jonas Tomaz <jonas.aquinos@hotmail.com>
2024-08-23 15:47:14 -0300

    feat: creating some location details on TourDetailComponent

--- frontend/src/components/TourDetailComponent/TourDetailComponent.tsx
+++ frontend/src/components/TourDetailComponent/TourDetailComponent.tsx
@@ -7,37 +7,44 @@ import Location_tourDetail from '../../assets/Location_tourDetail.png'
 const TourDetailComponent = () => {
   return (
     <div className={styles.mainContainer}>
-        <img src={Booking_image} className={styles.image} />
-        <div className={styles.placeDetails}>
-          <div className={styles.locate}>
-            <img src={Location_tourDetail} className={styles.icon} />
-            <span>Budapest, Hungary</span>
-            <p>View on map</p>
-          </div>
-          <div className={styles.share}>
-            <img src={Share_tourDetail} className={styles.icon} />
-            <img src={Heart_tourDetail} className={styles.icon} />
-          </div>
+      <div className={styles.imageContainer}>
+        <img src={Booking_image} className={styles.image} alt="Booking" />
+        <div className={styles.buttonContainer}>
+          <button className={styles.button}>Video</button>
+          <button className={styles.button}>Gallery</button>
         </div>
-        <h1>Wonders of the West Coast & Kimberly</h1>
-        <div>------------------------------</div>
-        <div>
-          <span>From</span>
-          <strong>$104</strong>
-          <span>Duration</span>
-          <strong>7 days</strong>
-          <span>Max People</span>
-          <strong>25</strong>
-          <span>Min Age</span>
-          <strong>12+</strong>
-          <span>Tour Type</span>
-          <strong>Adventure, Beaches</strong>
-          <span>Reviews</span>
-          <strong>S4.8 (15 Reviews)</strong>
-          <h2>Overview</h2>
-          <p>Istanbul, the vibrant and historic city straddling the continents of Europe and Asia, offers an energizing blend of cultures, sights, and experiences that captivate every traveler’s heart. As Turkey’s cultural and economic hub, Istanbul seamlessly fuses its rich heritage with modernity, creating an unforgettable journey for visitors.</p>
-          <h2>Map</h2>
+      </div>
+      <div className={styles.placeDetails}>
+        <div className={styles.locate}>
+          <img src={Location_tourDetail} className={styles.icon} alt="Location" />
+          <span>Budapest, Hungary</span>
+          <p>View on map</p>
         </div>
+        <div className={styles.share}>
+          <img src={Share_tourDetail} className={styles.icon} alt="Share" />
+          <img src={Heart_tourDetail} className={styles.icon} alt="Heart" />
+        </div>
+      </div>
+      <h1 className={styles.titleH1}>Wonders of the West Coast & Kimberly</h1>
+      <div className={styles.horizontalLine}></div>
+      <div>
+        <span>From</span>
+        <strong>$104</strong>
+        <span>Duration</span>
+        <strong>7 days</strong>
+        <span>Max People</span>
+        <strong>25</strong>
+        <span>Min Age</span>
+        <strong>12+</strong>
+        <span>Tour Type</span>
+        <strong>Adventure, Beaches</strong>
+        <span>Reviews</span>
+        <strong>4.8 (15 Reviews)</strong>
+        <h2>Overview</h2>
+        <p>Istanbul, the vibrant and historic city straddling the continents of Europe and Asia, offers an energizing blend of cultures, sights, and experiences that captivate every traveler’s heart. As Turkey’s cultural and economic hub, Istanbul seamlessly fuses its rich heritage with modernity, creating an unforgettable journey for visitors.</p>
+        <p>The city is home to some of the world’s most iconic landmarks, including the awe-inspiring Hagia Sophia, the majestic Blue Mosque, and the grand Topkapi Palace, each bearing witness to Istanbul’s illustrious past. Wandering through the bustling streets, you’ll find an array of delightful bazaars, where you can haggle for unique souvenirs, immerse yourself in the aromatic spices, and savor traditional Turkish delights.</p>
+        <h2>Map</h2>
+      </div>
     </div>
   )
 }
