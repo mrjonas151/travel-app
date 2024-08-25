6f3996928672bc0e3230dfd70bf3ee2b30cabb62
Jonas Tomaz <jonas.aquinos@hotmail.com>
2024-08-24 22:11:38 -0300

    feat: finishing DestinationDetailComponent

--- frontend/src/components/DestinationDetailComponent/DestinationDetailComponent.tsx
+++ frontend/src/components/DestinationDetailComponent/DestinationDetailComponent.tsx
@@ -1,7 +1,18 @@
 import styles from './DestinationDetailComponent.module.css';
 import maldives from '../../assets/maldives.jpg';
+import { FaArrowRight } from 'react-icons/fa';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import PopularToursComponent from '../PopularToursComponent/PopularToursComponent';
+import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
 
 const DestinationDetailComponent = () => {
+
+  const {isLoaded} = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: import.meta.env.VITE_APP_GOOGLE_MAPS_API_KEY || '',
+  })
+
   return (
     <div className={styles.mainContainer}>
       <div className={styles.rowContent}>
@@ -15,14 +26,128 @@ const DestinationDetailComponent = () => {
           </div>
         
         </div>
-        <div className={styles.map}>
+        <div className={styles.mapAndWeather}>
           <h2>City map</h2>
+          <div className={styles.map}>
+            {isLoaded ? (
+              <GoogleMap
+                mapContainerStyle={{width: '405px', height: '100%'}}
+                center={{
+                  lat: -27.590824,
+                  lng: -48.551262
+                }}
+                zoom={15}
+              >
+                <Marker 
+                  position={{ lat: -27.590824, lng: -48.551262 }}
+                  options={{ 
+                    icon: {
+                      url: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+                    }
+                  }}
+                />
+              </GoogleMap>
+            ) : (
+              <></>
+            )}
+          </div>
+          <div className={styles.weather}>
+            <h2>Weather</h2>
+            <div className={styles.rowItemsWeather}>
+              <span>Jan-Feb</span>
+              <p>12°-18°C</p>
+            </div>
+            <div className={styles.horizontalLine}></div>
+            <div className={styles.rowItemsWeather}>
+              <span>Mar-Apr</span>
+              <p>12°-18°C</p>
+            </div>
+            <div className={styles.horizontalLine}></div>
+            <div className={styles.rowItemsWeather}>
+              <span>May-Jun</span>
+              <p>12°-18°C</p>
+            </div>
+            <div className={styles.horizontalLine}></div>
+            <div className={styles.rowItemsWeather}>
+              <span>Jul-Aug</span>
+              <p>12°-18°C</p>
+            </div>
+            <div className={styles.horizontalLine}></div>
+            <div className={styles.rowItemsWeather}>
+              <span>Sep-Oct</span>
+              <p>12°-18°C</p>
+            </div>
+            <div className={styles.horizontalLine}></div>
+            <div className={styles.rowItemsWeather}>
+              <span>Nov-Dec</span>
+              <p>12°-18°C</p>
+            </div>
+          </div>
         </div>
       </div>
-      <h2>About UK</h2>
-      <p>Istanbul, the vibrant and historic city straddling the continents of Europe and Asia, offers an energizing blend of cultures, sights, and experiences that captivate every traveler’s heart. As Turkey’s cultural and economic hub, Istanbul seamlessly fuses its rich heritage with modernity, creating an unforgettable journey for visitors.</p>
-      <p>The city is home to some of the world’s most iconic landmarks, including the awe-inspiring Hagia Sophia, the majestic Blue Mosque, and the grand Topkapi Palace, each bearing witness to Istanbul’s illustrious past. Wandering through bustling streets, you’ll find an array of delightful bazaars where you can haggle for unique souvenirs, immerse yourself in aromatic spices, and savor traditional Turkish delights. As the sun sets, head to the banks of the Bosphorus strait and savor the view of Istanbul’s skyline, a true testament to its allure as both a crossroads between Europe and Asia. Prepare your taste buds for an unforgettable gastronomic adventure in Istanbul, where a delightful fusion of flavors awaits you at every turn.</p>
-      <h2>Basic Information</h2>
+      <div className={styles.aboutContainer}>
+        <h2>About UK</h2>
+        <p>Istanbul, the vibrant and historic city straddling the continents of Europe and Asia, offers an energizing blend of cultures, sights, and experiences that captivate every traveler’s heart. As Turkey’s cultural and economic hub, Istanbul seamlessly fuses its rich heritage with modernity, creating an unforgettable journey for visitors.</p>
+        <p>The city is home to some of the world’s most iconic landmarks, including the awe-inspiring Hagia Sophia, the majestic Blue Mosque, and the grand Topkapi Palace, each bearing witness to Istanbul’s illustrious past. Wandering through bustling streets, you’ll find an array of delightful bazaars where you can haggle for unique souvenirs, immerse yourself in aromatic spices, and savor traditional Turkish delights. As the sun sets, head to the banks of the Bosphorus strait and savor the view of Istanbul’s skyline, a true testament to its allure as both a crossroads between Europe and Asia. Prepare your taste buds for an unforgettable gastronomic adventure in Istanbul, where a delightful fusion of flavors awaits you at every turn.</p>
+      </div>
+      <div className={styles.basicContainer}>
+        <h2>Basic Information</h2>
+        <div className={styles.infos}>
+          <div className={styles.rowItems}>
+            <p>Country</p>
+            <strong>Turkey</strong>
+          </div>
+          <div className={styles.rowItems}>
+            <p>Language</p>
+            <strong>Turkish, Kurdish, Arabic, Zaza</strong>
+          </div>
+          <div className={styles.rowItems}>
+            <p>Currency</p>
+            <strong>Turkish, Lira</strong>
+          </div>
+          <div className={styles.rowItems}>
+            <p>Area</p>
+            <strong>2,036 Square Miles</strong>
+          </div>
+          <div className={styles.rowItems}>
+            <p>Population</p>
+            <strong>15M</strong>
+          </div>
+          <div className={styles.rowItems}>
+            <p>Time Zone</p>
+            <strong>UTC+2</strong>
+          </div>
+          <div className={styles.rowItems}>
+            <p>Time to Travel</p>
+            <strong>May, June, July, August</strong>
+          </div>
+        </div>
+      </div>
+      
+      <div className={styles.seeAll}>
+        <h1>Popular Tours in UK</h1>
+        <button>See All<FaArrowRight className={styles.icon}/></button>
+      </div>
+      <div className={styles.swiperWrapper}>
+            <div className={styles.popularToursCarousel}>
+              <Swiper
+                spaceBetween={30}
+                slidesPerView={4}
+                pagination={{ clickable: true }}
+                modules={[Pagination]}
+                className={styles.swiperContainer}
+              >
+              <SwiperSlide><PopularToursComponent /></SwiperSlide>
+              <SwiperSlide><PopularToursComponent /></SwiperSlide>
+              <SwiperSlide><PopularToursComponent /></SwiperSlide>
+              <SwiperSlide><PopularToursComponent /></SwiperSlide>
+              <SwiperSlide><PopularToursComponent /></SwiperSlide>
+              <SwiperSlide><PopularToursComponent /></SwiperSlide>
+
+              </Swiper>
+              <div className="swiper-pagination"></div>
+            </div>
+          </div>
     </div>
   )
 }
