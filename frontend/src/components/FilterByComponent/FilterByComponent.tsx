68fc553ef931ffee17b7e1a7eac3b377c838b0a4
Jonas Tomaz <jonas.aquinos@hotmail.com>
2024-08-28 15:53:09 -0300

    feat: adding price filter logic

--- frontend/src/components/FilterByComponent/FilterByComponent.tsx
+++ frontend/src/components/FilterByComponent/FilterByComponent.tsx
@@ -1,39 +1,46 @@
-import styles from './FilterByComponent.module.css'
+import styles from './FilterByComponent.module.css';
 import { useState } from 'react';
 
-const FilterByComponent = () => {
+interface FilterByComponentProps {
+  onPriceChange: (price: number) => void;
+}
 
-    const [price, setPrice] = useState(0);
+const FilterByComponent: React.FC<FilterByComponentProps> = ({ onPriceChange }) => {
+  const [price, setPrice] = useState(100000);
 
-    const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const value = Number(event.target.value);
-        setPrice(value);
-        const percentage = (value / 100000) * 100;
-
-        event.target.style.background = `linear-gradient(to right, #f84d50 ${percentage}%, #ddd ${percentage}%)`;
+  const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = Number(event.target.value);
+    setPrice(value);
+    const percentage = (value / 100000) * 100;
+    event.target.style.background = `linear-gradient(to right, #f84d50 ${percentage}%, #ddd ${percentage}%)`;
   };
 
-    return (
-        <div className={styles.mainContainer}>
-        <h2 className={styles.filterHeader}>Filter By</h2>
-        <div className={styles.sliderContainer}>
-            <input
-            type="range"
-            min="0"
-            max="100000"
-            step="500"
-            value={price}
-            onInput={handleSliderChange}
-            className={styles.slider}
-            />
-        </div>
-        <div className={styles.priceRange}>
-            <span>$0.00</span>
-            <strong>${(price / 100).toFixed(2)}</strong>
-        </div>
-        <button className={styles.submitButton}>Submit</button>
-        </div>
-    )
-}
+  const handleSubmit = () => {
+    onPriceChange(price/100);
+    console.log(price/100);
+  };
+
+  return (
+    <div className={styles.mainContainer}>
+      <h2 className={styles.filterHeader}>Filter By</h2>
+      <div className={styles.sliderContainer}>
+        <input
+          type="range"
+          min="0"
+          max="100000"
+          step="500"
+          value={price}
+          onChange={handleSliderChange}
+          className={styles.slider}
+        />
+      </div>
+      <div className={styles.priceRange}>
+        <span>$0.00</span>
+        <strong>${(price / 100).toFixed(2)}</strong>
+      </div>
+      <button className={styles.submitButton} onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+};
 
 export default FilterByComponent;
--- frontend/src/pages/TourPackage/TourPackage.tsx
+++ frontend/src/pages/TourPackage/TourPackage.tsx
@@ -1,34 +1,33 @@
-import { useEffect, useState } from "react"
-import BackgroundComponent from "../../components/BackgroundComponent/BackgroundComponent"
-import CategoriesComponent from "../../components/CategoriesComponent/CategoriesComponent"
-import DestinationsFilterComponent from "../../components/DestinationsFilterComponent/DestinationsFilterComponent"
-import FilterByComponent from "../../components/FilterByComponent/FilterByComponent"
-import Footer from "../../components/Footer/Footer"
-import Header from "../../components/Header/Header"
-import PopularToursComponent from "../../components/PopularToursComponent/PopularToursComponent"
-import ReviewFilterComponent from "../../components/ReviewFilterComponent/ReviewFilterComponent"
-import SearchFilterComponent from "../../components/SearchFilterComponent/SearchFilterComponent"
-import SearchTravel from "../../components/SearchTravel/SearchTravel"
-import styles from './TourPackage.module.css'
-import api from "../../services/api"
-import { TourDetailComponentProps } from "../../components/TourDetailComponent/TourDetailComponent"
-import ReactPaginate from 'react-paginate'
+import { useEffect, useState } from "react";
+import BackgroundComponent from "../../components/BackgroundComponent/BackgroundComponent";
+import CategoriesComponent from "../../components/CategoriesComponent/CategoriesComponent";
+import DestinationsFilterComponent from "../../components/DestinationsFilterComponent/DestinationsFilterComponent";
+import FilterByComponent from "../../components/FilterByComponent/FilterByComponent";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import PopularToursComponent from "../../components/PopularToursComponent/PopularToursComponent";
+import ReviewFilterComponent from "../../components/ReviewFilterComponent/ReviewFilterComponent";
+import SearchFilterComponent from "../../components/SearchFilterComponent/SearchFilterComponent";
+import SearchTravel from "../../components/SearchTravel/SearchTravel";
+import styles from './TourPackage.module.css';
+import api from "../../services/api";
+import { TourDetailComponentProps } from "../../components/TourDetailComponent/TourDetailComponent";
+import ReactPaginate from 'react-paginate';
 
 const TourPackage = () => {
   const [tours, setTours] = useState<TourDetailComponentProps[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
-
   const [currentPage, setCurrentPage] = useState(0);
   const toursPerPage = 9;
-
   const [sortOption, setSortOption] = useState('popularity');
+  const [priceFilter, setPriceFilter] = useState<number>(100000);
 
   const getAllTours = async () => {
     setLoading(true);
     try {
       const response = await api.get('/tourDetails');
-      setTours(response.data);  
+      setTours(response.data);
     } catch (err) {
       setError('Error searching for tours');
     } finally {
@@ -50,17 +49,29 @@ const TourPackage = () => {
     return sorted;
   };
 
+  const filteredTours = sortedTours().filter(tour => {
+    const price = Number(tour.initial_price);
+    const filterPrice = Number(priceFilter);
+
+    return price <= filterPrice;
+  });
+  
   const offset = currentPage * toursPerPage;
-  const currentTours = sortedTours().slice(offset, offset + toursPerPage);
-  const pageCount = Math.ceil(tours.length / toursPerPage);
+  const currentTours = filteredTours.slice(offset, offset + toursPerPage);
+  const pageCount = Math.ceil(filteredTours.length / toursPerPage);
 
   const handlePageClick = (selectedItem: { selected: number }) => {
     setCurrentPage(selectedItem.selected);
   };
 
   const handleSortChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setSortOption(e.target.value); 
-    setCurrentPage(0); 
+    setSortOption(e.target.value);
+    setCurrentPage(0);
+  };
+
+  const handlePriceFilterChange = (price: number) => {
+    setPriceFilter(price);
+    setCurrentPage(0);
   };
 
   if (loading) return <p>Waiting...</p>;
@@ -71,14 +82,14 @@ const TourPackage = () => {
       <Header />
       <BackgroundComponent />
       <div className={styles.searchContainer}>
-          <SearchTravel />
+        <SearchTravel />
       </div>
       <div className={styles.mainPage}>
         <div className={styles.content}>
           <div className={styles.rowContent}>
             <SearchFilterComponent />
             <div className={styles.rowFilters}>
-              <p>{tours.length} Tours</p>
+              <p>{filteredTours.length} Tours</p>
               <div>
                 <span>Sort by</span>
                 <select value={sortOption} onChange={handleSortChange}>
@@ -88,44 +99,42 @@ const TourPackage = () => {
                 </select>
               </div>
             </div>
-            
           </div>
-          
-          <FilterByComponent />
+          <FilterByComponent onPriceChange={handlePriceFilterChange} />
           <CategoriesComponent />
           <DestinationsFilterComponent />
           <ReviewFilterComponent />
         </div>
         <div className={styles.allTours}>
-            {currentTours.map((tour) => (
-              <div key={tour.id} className={styles.popularTourItem}>
-                <PopularToursComponent
-                  id={tour.id}
-                  url_image={tour.url_image}
-                  city={tour.city}
-                  country={tour.country}
-                  title={tour.title}
-                  averageRating={tour.averageRating}
-                  userRatings={tour.userRatings}
-                  initial_date={tour.initial_date}
-                  final_date={tour.final_date}
-                  initial_price={tour.initial_price} 
-                  max_people={tour.max_people} 
-                  min_age={tour.min_age} 
-                  tour_type={tour.tour_type} 
-                  overview_city={tour.overview_city} 
-                  overview_curiosities={tour.overview_curiosities} 
-                  latitude={tour.latitude} 
-                  longitude={tour.longitude} 
-                  category={{
-                    id: tour.category.id,
-                    title: tour.category.title,
-                    tour_quantity: tour.category.tour_quantity,
-                    price: tour.category.price
-                  }}                
-                />
-              </div>
-            ))}
+          {currentTours.map((tour) => (
+            <div key={tour.id} className={styles.popularTourItem}>
+              <PopularToursComponent
+                id={tour.id}
+                url_image={tour.url_image}
+                city={tour.city}
+                country={tour.country}
+                title={tour.title}
+                averageRating={tour.averageRating}
+                userRatings={tour.userRatings}
+                initial_date={tour.initial_date}
+                final_date={tour.final_date}
+                initial_price={tour.initial_price}
+                max_people={tour.max_people}
+                min_age={tour.min_age}
+                tour_type={tour.tour_type}
+                overview_city={tour.overview_city}
+                overview_curiosities={tour.overview_curiosities}
+                latitude={tour.latitude}
+                longitude={tour.longitude}
+                category={{
+                  id: tour.category.id,
+                  title: tour.category.title,
+                  tour_quantity: tour.category.tour_quantity,
+                  price: tour.category.price
+                }}
+              />
+            </div>
+          ))}
         </div>
         <ReactPaginate
           previousLabel={"<"}
@@ -140,10 +149,9 @@ const TourPackage = () => {
           activeClassName={styles.active}
         />
       </div>
-      
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default TourPackage
+export default TourPackage;
