700f73341161fb8d4ef7d766b511576aa0ab2cf1
Jonas Tomaz <jonas.aquinos@hotmail.com>
2024-08-30 17:47:04 -0300

    feat: changing image background to a specific country

--- frontend/src/components/BackgroundComponent/BackgroundComponent.tsx
+++ frontend/src/components/BackgroundComponent/BackgroundComponent.tsx
@@ -3,13 +3,14 @@ import TextBackgroundComponent from '../TextBackgroundComponent/TextBackgroundCo
 
 interface BackgroundProps {
   country?: string;
+  image?: string;
 }
 
-const BackgroundComponent = ({country}:BackgroundProps) => {
+const BackgroundComponent = ({country, image}:BackgroundProps) => {
   return (
     <div>
         <div className={styles.imageContainer}>
-          <img src="https://firebasestorage.googleapis.com/v0/b/travel-app-d9bdb.appspot.com/o/maldives.jpg?alt=media&token=3a94bac7-eb1b-4883-a6fa-1b19902a9ab5" alt="Air Balloon" />
+          <img src={image || "https://firebasestorage.googleapis.com/v0/b/travel-app-d9bdb.appspot.com/o/maldives.jpg?alt=media&token=3a94bac7-eb1b-4883-a6fa-1b19902a9ab5"} alt="Air Balloon" />
           <div className={styles.textOverlay}>
             <TextBackgroundComponent title={'Tour Package'} country={country || ''}/>
           </div>
